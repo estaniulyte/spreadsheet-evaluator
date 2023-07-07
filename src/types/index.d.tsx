72e8edff5df543787ed4632d0d22aa2ee5fcfdc4
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-07 16:48:28 +0300

    feat: add ComputedResult type

--- src/types/index.d.tsx
+++ src/types/index.d.tsx
@@ -2,3 +2,5 @@ export type CellValue = string | number | boolean;
 export type SpreadsheetData = SpreadsheetObject[];
 export type SpreadsheetObject = {id: String, data: CellValue[][]};
 
+export type ComputedResult = SpreadsheetObject[];
+
