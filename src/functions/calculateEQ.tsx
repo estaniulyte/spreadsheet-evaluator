762e2ce17bb7772bfde6e8f8205bf8d464afe1c8
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-09 15:14:01 +0300

    change: rewrite EQ function to be more reusable

--- src/functions/calculateEQ.tsx
+++ src/functions/calculateEQ.tsx
@@ -1,28 +1,21 @@
 import { SpreadsheetObject } from "../types/index.d";
 
-import { getLetterIndex } from "./getLetterIndex";
+import { getItemFromSpreadsheet } from "./getItemFromSpreadsheet";
 
 export function calculateEQ(
-  formula: string,
+  eqElements: Array<string>,
   spreadsheet: SpreadsheetObject
 ): boolean | string {
-  var eqElements = formula.split(/[(,)]/).map((item) => item.trim()); // remove empty spaces
-  eqElements.shift(); // remove function name
-  eqElements.pop(); // remove empty space
   if (eqElements.length !== 2)
     return "#ERROR: Two values excepted. Got " + eqElements.length;
   var firstValue: number = 0;
   let secondValue: number = 1;
   if (parseInt(eqElements[0])) firstValue = parseInt(eqElements[0]);
   else
-    firstValue = spreadsheet.data[parseInt(eqElements[0].charAt(1)) - 1][
-      getLetterIndex(eqElements[0].charAt(0))
-    ] as number;
+    firstValue = getItemFromSpreadsheet(spreadsheet, eqElements[0]) as number;
   if (parseInt(eqElements[1])) secondValue = parseInt(eqElements[1]);
   else
-    secondValue = spreadsheet.data[parseInt(eqElements[1].charAt(1)) - 1][
-      getLetterIndex(eqElements[1].charAt(0))
-    ] as number;
+    secondValue = getItemFromSpreadsheet(spreadsheet, eqElements[1]) as number;
 
   if (firstValue === secondValue) return true;
   else return false;
