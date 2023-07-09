9ed9b01d21bb9325e73a8c5186e7604782bb1384
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-09 15:02:27 +0300

    feat: create getItemFromSpreadsheet function

---
+++ src/functions/getItemFromSpreadsheet.tsx
@@ -0,0 +1,8 @@
+import { SpreadsheetObject, CellValue } from "../types/index.d";
+import { getLetterIndex } from "./getLetterIndex";
+
+export function getItemFromSpreadsheet(spreadsheet: SpreadsheetObject, item: string): CellValue{
+  return spreadsheet.data[parseInt(item.charAt(1)) - 1][
+    getLetterIndex(item.charAt(0))
+  ];
+}
