d6aa2d50936fbdebde0281708ea0685ba04840a8
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-09 15:16:34 +0300

    change: rewrite OR function to be more reusable

--- src/functions/calculateOR.tsx
+++ src/functions/calculateOR.tsx
@@ -1,23 +1,16 @@
 import { SpreadsheetObject } from "../types/index.d";
 
-import { getLetterIndex } from "./getLetterIndex";
+import { getItemFromSpreadsheet } from "./getItemFromSpreadsheet";
 import { checkCellA1Format } from "./checkCellA1Format";
 
 export function calculateOR(
-  formula: string,
+  orElements: Array<string>,
   spreadsheet: SpreadsheetObject
 ): boolean | string {
-  var orElements = formula.split(/[(,)]/).map((item) => item.trim()); // remove empty spaces
-  orElements.shift(); // remove function name
-  orElements.pop(); // remove empty space
-
   let isTrue: boolean | string = false;
   orElements.forEach(function (item) {
     if (checkCellA1Format(item)) {
-      var val =
-        spreadsheet.data[parseInt(item.charAt(1)) - 1][
-          getLetterIndex(item.charAt(0))
-        ];
+      var val = getItemFromSpreadsheet(spreadsheet, item);
       if (typeof val !== "boolean")
         isTrue = "#ERROR: one of provided values is not boolean";
       else if (val === true) isTrue = true;
