096ce066781bab1aec3666b28620c6a80541f841
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-09 16:05:48 +0300

    change: rewrite NOT function to be more reusable

--- src/functions/calculateNOT.tsx
+++ src/functions/calculateNOT.tsx
@@ -1,21 +1,15 @@
 import { SpreadsheetObject } from "../types/index.d";
 
-import { getLetterIndex } from "./getLetterIndex";
+import { getItemFromSpreadsheet } from "./getItemFromSpreadsheet";
 
 export function calculateNOT(
-  formula: string,
+  notElements: Array<string>,
   spreadsheet: SpreadsheetObject
 ): boolean | string {
-  var notElements = formula.split(/[(,)]/).map((item) => item.trim()); // remove empty spaces
-  notElements.shift(); // remove function name
-  notElements.pop(); // remove empty space
   if (notElements.length !== 1)
     return "#ERROR: One value excepted. Got " + notElements.length;
   var value: boolean = false;
   if (parseInt(notElements[0])) value = Boolean(notElements[0]);
-  else
-    value = spreadsheet.data[parseInt(notElements[0].charAt(1)) - 1][
-      getLetterIndex(notElements[0].charAt(0))
-    ] as boolean;
+  else value = getItemFromSpreadsheet(spreadsheet, notElements[0]) as boolean;
   return !value;
 }
