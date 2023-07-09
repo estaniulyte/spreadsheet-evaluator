f923fdd58ea61b514d7c6657e77a4eae14b2cc4e
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-09 15:07:20 +0300

    cleanup: fix code format

--- src/functions/getItemFromSpreadsheet.tsx
+++ src/functions/getItemFromSpreadsheet.tsx
@@ -1,7 +1,10 @@
 import { SpreadsheetObject, CellValue } from "../types/index.d";
 import { getLetterIndex } from "./getLetterIndex";
 
-export function getItemFromSpreadsheet(spreadsheet: SpreadsheetObject, item: string): CellValue{
+export function getItemFromSpreadsheet(
+  spreadsheet: SpreadsheetObject,
+  item: string
+): CellValue {
   return spreadsheet.data[parseInt(item.charAt(1)) - 1][
     getLetterIndex(item.charAt(0))
   ];
