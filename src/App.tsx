90414387ce9bea9cb17d331865ea8fb98544ccc3
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-11 13:53:00 +0300

    feat: add Alerts in functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,49 @@
-import './App.css';
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 import { SpreadsheetData } from "./types/index.d";
 
 import { computeSpreadsheet } from "./functions/computeSpreadsheet";
 
 import { apiInstance } from "./api/api";
+import Alert from "./components/Alert";
 
 function App() {
   const [spreadsheetData, setSpreadsheetData] = useState<SpreadsheetData>([]);
   const [computedResult, setComputedResult] = useState<SpreadsheetData>([]);
+  const [alerts, setAlerts] = useState(Array<{ type: string; text: string }>);
+
+  const addAlert = useCallback((text: string, type: string) => {
+    const isDuplicate = alerts.some((alert) => alert.text === text && alert.type === type);
+    
+    if (!isDuplicate) {
+      const newAlert = { text, type };
+      
+      setAlerts((prevAlerts) => [newAlert, ...prevAlerts]);
+    }
+  }, [alerts]);
 
   useEffect(() => {
     const fetchSpreadsheetData = async () => {
       try {
         const response = await apiInstance.get("/sheets");
         const fetchedData = response.data;
+        addAlert("Spreadsheet data fetched.", "Success");
         setSpreadsheetData(fetchedData.sheets);
       } catch (error) {
+        alerts.push({ text: "Error fetching spreadsheet data: " + error, type: "Error" });
+        setAlerts(alerts);
         console.error("Error fetching spreadsheet data:", error);
       }
     };
 
     fetchSpreadsheetData();
-  }, []);
+  });
 
   useEffect(() => {
     var computedData = computeSpreadsheet(spreadsheetData);
     setComputedResult(computedData);
-  }, [spreadsheetData]);
+    addAlert("Spreadsheet data computed.", "Success");
+  }, [spreadsheetData, addAlert]);
 
   const submitResults = async () => {
     const data = {
@@ -40,8 +55,10 @@ function App() {
       .post("/verify/eyJ0YWdzIjpbXX0", data)
       .then((response) => {
         console.log("Request successful:", response.data);
+        addAlert("Spreadsheet data submited. Response: " + response.data.message, "Success");
       })
       .catch((error) => {
+        addAlert("Spreadsheet data submit failed. Error: " + error, "Error");
         console.error("Error:", error);
       });
   };
