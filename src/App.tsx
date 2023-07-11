6868ff47d065bc940aeb0684d3c1ef78e6883a18
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-07-11 13:53:10 +0300

    feat: add UI

--- src/App.tsx
+++ src/App.tsx
@@ -64,21 +64,46 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="">
+      <nav className="bg-gray-800 mb-6">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="https://www.wix.com/"
+            className="flex items-center"
+            target="blank"
+          >
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              WIX Task
+            </span>
+          </a>
+          <div className="w-ful md:w-auto text-white">
+            by{" "}
+            <a
+              href="https://estaniulyte.com/"
+              className="hover:text-blue-300 cursor:pointer"
+              target="blank"
+            >
+              Evelina Staniulytė
+            </a>
+          </div>
+        </div>
+      </nav>
+      <div className="max-w-screen-xl flex flex-col justify-center justify-items-center text-center mx-auto">
+        <h1 className="mb-4 self-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl">
+          Spreadsheet Evaluator
+        </h1>
+        <button
+          onClick={submitResults}
+          className="mx-auto my-11 w-40 px-5 py-3 text-base font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 "
         >
-          Learn React
-        </a>
-      </header>
+          Submit Results
+        </button>
+        <div className="mt-4">
+          {alerts.map((item, index) => {
+            return <Alert key={index} type={item.type} text={item.text} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
